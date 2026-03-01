f91575f3fca54080fd4816a03c39fc2532913801
sinofseven <em.s.00001@gmail.com>
2026-03-02 02:33:12 +0900

    fix format

--- src/controllers/queue/queue.ts
+++ src/controllers/queue/queue.ts
@@ -1,8 +1,13 @@
+export type Task = {
+  url: string;
+};
+
 export async function queue(
-  batch: MessageBatch,
+  batch: MessageBatch<Task>,
   env: Env,
   ctx: ExecutionContext,
 ): Promise<void> {
+  console.log({ env, ctx });
   for (const message of batch.messages) {
     console.log({
       id: message.id,
